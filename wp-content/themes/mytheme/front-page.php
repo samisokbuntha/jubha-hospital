82cabc019df61f525c8b64fccb77ea3590e4145b
SAMI Sokbuntha <samisokbuntha@gmail.com>
2026-01-09 07:21:40 +0700

    test-d1

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -1,1 +1,11 @@
-<h1>kobsari yk-test</h1>
+<?php get_header(); ?>
+
+    <div class="hero-section">
+        <div class="bg-black"></div>
+        <div class="hero-content">
+            <h1>Welcome to Jubha Hospital</h1>
+            <p>Providing compassionate, high-quality healthcare for everyone.</p>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,8 +1,2 @@
-<?php
-function my_theme_enqueue_styles() {
-    wp_enqueue_style(
-        'my-theme-style',
-        get_stylesheet_uri()
-    );
-}
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+
